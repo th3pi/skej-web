eebda2afe8fb7f5c715435e7bf5d6d0bad3fdf2f
th3pi <th3pizzaman15@gmail.com>
2021-01-27 19:10:34 -0500

    added 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -4,26 +4,26 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routes = [{
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '*',
+        name: '404',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "404" */ '../views/404.vue')
+    }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
---
+++ src/views/404.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div id="logo">ÉRROR 404</div>
+    <input
+      type="button"
+      class="button"
+      value="Show me the way"
+      onclick="window.location.replace('https://skej.bhyn.dev/')"
+    />
+  </div>
+</template>
